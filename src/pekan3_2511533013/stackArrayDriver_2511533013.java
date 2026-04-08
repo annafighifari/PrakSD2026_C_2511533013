1a5d1331c71dd5812975cfb58582c5ab9b3bf58f
annaf <annaf@DESKTOP-9JE232O>
2026-04-08 18:48:31 +0700

    praktikum pekan 3

--- src/pekan3_2511533013/stackArrayDriver_2511533013.java
+++ src/pekan3_2511533013/stackArrayDriver_2511533013.java
@@ -2,4 +2,14 @@ package pekan3_2511533013;
 
 public class stackArrayDriver_2511533013 {
 
+	public static void main(String[] args) {
+		stackArray_2511533013 s_3013 = new stackArray_2511533013();
+		s_3013.push_3013 (10);
+		s_3013.push_3013(20);
+		s_3013.push_3013(30);
+		System.out.println(s_3013.pop() + " dikeluarkan dari stack");
+		System.out.println("Elemen teratas adalah: " + s_3013.peek());
+		System.out.println("Elemen pada stack: ");
+		s_3013.print();
+	}
 }
--- src/pekan3_2511533013/stackArray_2511533013.java
+++ src/pekan3_2511533013/stackArray_2511533013.java
@@ -1,5 +1,60 @@
 package pekan3_2511533013;
 
 public class stackArray_2511533013 {
-
-}
+	static final int MAX_3013 = 1000;
+	int top_3013;
+	int a[] = new int[MAX_3013];
+	boolean isEmpty()
+	{
+		return (top_3013 < 0);
+	}
+	stackArray_2511533013()
+	{
+		top_3013 = -1;
+		
+	}
+	boolean push_3013 (int x)
+	{
+		if (top_3013 >= (MAX_3013 -1)) {
+			System.out.println("Stack Overflow");
+			return false;
+			
+		}
+		else {
+			a[++top_3013] = x;
+			System.out.println(x + " dimasukkan dalam Stack");
+			return true;
+			
+		}
+	}
+	int pop()
+	{
+		if(top_3013 < 0) {
+			System.out.println("Stack Underflow");
+			return 0;
+	
+		}else {
+				int x = a [top_3013--];
+				return x;
+			}
+		
+		}
+	int peek()
+	{
+		if (top_3013 < 0) {
+			System.out.println("Stack Underflow");
+			return 0;
+			
+		}
+		else {
+			int x = a [top_3013];
+			return x;
+		}
+	}
+	void print  () {
+		for (int i = top_3013; i>-1; i--) {
+			System.out.print(" "+ a[i]);
+		}
+		
+		}
+	}
